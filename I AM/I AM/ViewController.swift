905035a0f1ee0bda3666d22ce74002c4fe326eef
Kang Cheng <me@chengkang.pw>
2016-11-03 18:48:44 +0000

    init project

---
+++ I AM/I AM/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  I AM
+//
+//  Created by Ant on 2016/11/3.
+//  Copyright © 2016年 Lahk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
